85a2b6df4a67dacc647310f1f4e9cd192e72e943
Gupta_A <anishgupta7500@gmail.com>
2025-10-21 09:08:49 -0700

    initial commits

--- assets/js/mansionGame/mansionLevel6.js
+++ assets/js/mansionGame/mansionLevel6.js
@@ -1,2 +1,24 @@
-import GameEnvBackground from './GameEngine/<path>.js'
-import AnimatedPlayer from './GameEngine/<path>.js'
+import GameEnvBackground  from "./GameEngine/GameEnvBackground.js";
+import Player from "./GameEngine/Player.js";
+import Npc from './GameEngine/Npc.js';
+import GameControl from './GameEngine/GameControl.js';
+import MansionLevel6_BattleRoom from './MansionLevel6_BattleRoom.js';
+
+class MansionLevel6 {
+    constructor(gameEnv){
+        let width = gameEnv.innerWidth;
+        let height = gameEnv.innerHeight;
+        let path = gameEnv.path;
+
+        const image_src_bossIntroChamber = path + "images/gamify/bgBossIntroChamber.png"
+        const image_bossIntroChamber = {
+            name: 'bossintro',
+            greeting: "You hear a faint echo from behind the ebony doors.",
+            src: image_src_bossIntroChamber,
+            pixels: {height: 341, width: 498}
+        };
+
+    }
+}
+
+export default MansionLevel6;
---
+++ assets/js/mansionGame/mansionLevel6_BattleRoom.js
@@ -0,0 +1,14 @@
+import GameEnvBackground  from "./GameEngine/GameEnvBackground.js";
+import Player from "./GameEngine/Player.js";
+import Npc from './GameEngine/Npc.js';
+import GameControl from './GameEngine/GameControl.js';
+
+class MansionLevel6_BattleRoom {
+    constructor(gameEnv){
+        let width = gameEnv.innerWidth;
+        let height = gameEnv.innerHeight;
+        let path = gameEnv.path;
+    }
+}
+
+export default MansionLevel6_BattleRoom;
