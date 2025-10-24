d75c26b23ee5957fd56f6f58f0cd638dd3ff19e6
Gupta_A <anishgupta7500@gmail.com>
2025-10-24 09:14:39 -0700

    fix background dimensions to match basic game

--- assets/js/mansionGame/mansionLevel6.js
+++ assets/js/mansionGame/mansionLevel6.js
@@ -20,7 +20,7 @@ class MansionLevel6 {
             name: 'bossintro',
             greeting: "You hear a faint echo from behind the ebony doors.",
             src: image_src_chamber,
-            pixels: {height: 341, width: 498}
+            pixels: {height: 580, width: 1038}
         };
 
         /*
