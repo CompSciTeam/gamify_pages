714256264bf2a2b9bc8ea34bbd9eea2c30462150
1j-bla1 <ejdlb08@gmail.com>
2025-10-27 09:23:24 -0700

    Changing the spawn point of the player to go on screen.

--- assets/js/mansionGame/mansionLevel6_BattleRoom.js
+++ assets/js/mansionGame/mansionLevel6_BattleRoom.js
@@ -23,7 +23,7 @@ class MansionLevel6_BattleRoom {
             SCALE_FACTOR: MC_SCALE_FACTOR,
             STEP_FACTOR: 500,
             ANIMATION_RATE: 30,
-            INIT_POSITION: {x: (width / 3), y: (1.1*height - (height/MC_SCALE_FACTOR))},
+            INIT_POSITION: {x: (width / 3), y: (height - (height/MC_SCALE_FACTOR))},
             pixels: {height: 3600, width: 1200},
             orientation: {rows: 1, columns: 3},
             // for now I'm just making the animation for all directions the same.
